9f655cfcb004de3ce45d5c1654d9d4d6b469b116
amybaldwindev <amybaldwindev@gmail.com>
2017-11-06 09:45:36 -0800

    add site hight to equal window, change css styles

---
+++ js/site.js
@@ -0,0 +1,13 @@
+(function($){
+
+	var windowHeight = $(window).height();
+	console.log("Window height= " + windowHeight);
+	// make div height half of window height
+	var sectHeight = windowHeight;
+	$(".sect").css("height", sectHeight);
+	
+	console.log("Grid height = " + gridHeight);
+	// get half hight of div
+	// get half height of h2
+	
+})(jQuery);
